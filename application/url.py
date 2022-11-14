527ce1cdbd2bef31311166ffc14727bf95cab5be
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-11-14 16:21:13 +0630

    update

--- application/url.py
+++ application/url.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from application import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('callback', views.callback.as_view(), name="callback")
 ]
--- application/views.py
+++ application/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from application.serializer import kbzRequestSerializer
 # Create your views here.
 
 
+class callback(APIView):
+    def post(self, request):
+        kbs = kbzRequestSerializer(data=request.data)
+        if kbs.is_valid():
+            kbs.save()
+            # return Response("success", content_type="text/plain")
+            return Response(kbs.data)
 
+        return Response(kbs.errors)
 
 
 def index(request):
